147f53b5c578b4777feef483775adf7f7511da18
Santi Espinosa <saigesp@gmail.com>
2020-03-28 21:06:13 +0100

    add temporally mocked api response

--- src/views/Scanner.vue
+++ src/views/Scanner.vue
@@ -145,6 +145,26 @@ export default {
       this.inProgress = true;
       document.getElementById('start').text = 'Procesando...'
       this.fakeInitiative = null
+
+      /**
+      * Mock API response
+      * Remove from here when work is done
+      */
+      const response = {"status": "SUCCESS", "result": {"topics": ["ODS 13 Acci\u00f3n por el clima", "ODS 14 Vida submarina", "ODS 3 Salud y bienestar", "ODS 7 Energ\u00eda asequible y no contaminante", "ODS 8 Trabajo decente y crecimiento econ\u00f3mico"], "tags": [{"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.1 Adaptaci\u00f3n frente a desastres naturales", "tag": "Crisis clim\u00e1tica", "times": 1}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.1 Adaptaci\u00f3n frente a desastres naturales", "tag": "Emergencia clim\u00e1tica", "times": 2}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.1 Adaptaci\u00f3n frente a desastres naturales", "tag": "Riesgos relacionados con el clima y los desastres naturales", "times": 2}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Acuerdo de Par\u00eds", "times": 1}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Calentamiento global", "times": 2}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Cambio clim\u00e1tico", "times": 15}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Cambio clim\u00e1tico e impacto", "times": 1}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Contaminaci\u00f3n atmosf\u00e9rica", "times": 1}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Descarbonizaci\u00f3n", "times": 1}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Efecto invernadero", "times": 2}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Emisiones de CO2", "times": 2}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Emisiones de gases de efecto invernadero", "times": 2}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Estrategia de Transici\u00f3n Justa", "times": 1}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Gases efecto invernadero", "times": 2}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Grupo Intergubernamental de Expertos sobre el Cambio Clim\u00e1tico (IPCC)", "times": 2}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "IPCC", "times": 1}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Ley de Cambio Clim\u00e1tico y Transici\u00f3n Energ\u00e9tica", "times": 1}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Plan Nacional Integrado de Energ\u00eda y Clima", "times": 3}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Transici\u00f3n energ\u00e9tica", "times": 1}, {"topic": "ODS 13 Acci\u00f3n por el clima", "subtopic": "13.2 Planes nacionales contra el cambio climatico", "tag": "Transici\u00f3n justa", "times": 1}, {"topic": "ODS 14 Vida submarina", "subtopic": "14.3 Acidificaci\u00f3n de los oc\u00e9anos", "tag": "Nivel del mar", "times": 1}, {"topic": "ODS 3 Salud y bienestar", "subtopic": "3.9 Productos qu\u00edmicos y contaminaci\u00f3n", "tag": "Enfermedad y factores ambientales", "times": 1}, {"topic": "ODS 3 Salud y bienestar", "subtopic": "3.9 Productos qu\u00edmicos y contaminaci\u00f3n", "tag": "Enfermedades causadas por la poluci\u00f3n", "times": 1}, {"topic": "ODS 7 Energ\u00eda asequible y no contaminante", "subtopic": "7.1 Acceso universal a energ\u00eda", "tag": "Ley de transici\u00f3n energ\u00e9tica", "times": 1}, {"topic": "ODS 7 Energ\u00eda asequible y no contaminante", "subtopic": "7.2 Aumento de energ\u00edas renovables", "tag": "Transici\u00f3n energ\u00e9tica", "times": 1}, {"topic": "ODS 8 Trabajo decente y crecimiento econ\u00f3mico", "subtopic": "8.5 Trabajo decente", "tag": "Creaci\u00f3n de empleo", "times": 1}, {"topic": "ODS 8 Trabajo decente y crecimiento econ\u00f3mico", "subtopic": "8.5 Trabajo decente", "tag": "Transici\u00f3n justa", "times": 1}]}}
+      this.result = response.result;
+      this.csvItems = this.result.tags
+      this.fakeInitiative = {
+        'topics': this.result.topics,
+        'tags': this.result.tags
+      }
+      this.inProgress = false;
+      document.getElementById('start').text = 'Iniciar proceso'
+      VueScrollTo.scrollTo('#result', 1500)
+      // Remove up to here when work is done
+
+      /**
+      * Uncomment this when work is done
+
       api.annotate(this.inputText)
         .then(response => {
           if (response.status==="SUCCESS") {
@@ -169,6 +189,7 @@ export default {
           this.inProgress = false;
           document.getElementById('start').text = 'Iniciar proceso'
         });
+        */
     },
     showComparison: function() {
 
