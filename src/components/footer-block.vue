80aa921e6aace677809c6fab6dff66d0ff58f7c8
pr3ssh <x@pr3ssh.net>
2020-06-29 09:17:31 +0200

    Remake footer

--- src/components/footer-block.vue
+++ src/components/footer-block.vue
@@ -1,40 +1,53 @@
 <template>
   <div>
     <div id="footer" class="o-container o-container--fluid o-section u-bg-secondary-light">
-      <div class="o-grid">
-        <div class="o-grid__col u-12@sm">
-          <div class="o-container">
+
+      <div class="o-container">
+        <div class="o-grid">
+          <div class="o-grid__col u-7@sm u-margin-top-4">
+            <p class="u-text-overline">Impulsado por</p>
             <div class="o-grid">
-              <div class="o-grid__col u-3@sm text-center">
-                <a href="/"><img src="/img/logo.png" class="big" alt="Logo de Parlamento 2030"></a>
-                <a href="http://politicalwatch.es" target="_blank"><img src="/img/politicalwatch.png" class="small" alt="Logo de Political Watch"></a>
+              <div class="o-grid__col u-7@sm">
+                <a href="http://www.exteriores.gob.es/Portal/es/Ministerio/SecretariosDeEstado/SecretariaDeEstadoCooperacionInternacional/Paginas/Inicio.aspx" target="_blank"><img src="/img/maeuec-seci.jpg" class="big" alt="Logo de la Secretaria de Estado de Cooperación (Ministerio de Asuntos Exteriores, Unión Europea y Cooperación) del Gobierno de España"></a>
               </div>
-              <div class="o-grid__col u-8@sm u-offset-1@sm u-text-tbody2">
-                <p>
-                <strong>Parlamento 2030</strong> es un proyecto de <a href="http://politicalwatch.es" target="_blank">Political Watch</a>, plataforma que se inscribe dentro del think-and-do tank <a href="http://ciecode.es" target="_blank">CIECODE</a>.
-                <br/>
-                Todo el código fuente está disponible en <a href="https://github.com/politicalwatch" target=_blank>Github</a> con licencia <a href="https://www.gnu.org/copyleft/gpl.html" target="_blank">GNU/GPLv3</a>.
-                <br/>
-                Contáctanos en <a href="mailto:parlamento2030@ciecode.es" target="_blank">parlamento2030@ciecode.es</a>
-                </p>
+              <div class="o-grid__col u-5@sm">
+                <a href="http://politicalwatch.es" target="_blank"><img src="/img/ciecode.png" class="big" alt="Logo de CIECODE"></a>
               </div>
             </div>
           </div>
+          <div class="o-grid__col u-4@sm u-offset-1@sm u-margin-top-4">
+            <p class="u-text-overline">Contacto</p>
+            <tipi-icon icon="github" class="c-icon--secondary-dark" />&nbsp;<a class="u-color-secondary-dark u-text-tbody2" href="http://github.com/politicalwatch/" title="github.com/politicalwatch" target="_blank">Consulta nuestro código en Github</a>
+            <br>
+            <tipi-icon icon="mail" class="c-icon--secondary-dark" />&nbsp;<a class="u-color-secondary-dark u-text-tbody2" href="mailto:parlamento2030@ciecode.es" title="parlamento2030@ciecode.es" target="_blank">Escribe a parlamento2030@ciecode.es</a>
+          </div>
+        </div>
+        <div class="o-grid">
+          <div class="o-grid__col u-12@sm u-margin-top-4">
+            <p class="u-color-secondary-dark u-text-tbody2">{{ (new Date()).getFullYear()  }} Political Watch. Algunos derechos reservados.</p>
+          </div>
         </div>
       </div>
+
     </div>
+
     <div class="c-decorator" style="background-image: url('/img/multicolor.jpg')"></div>
 
     <cookie-law theme="base" buttonText="Entendido" buttonClass="btn btn-custom" message="Este sitio usa cookies para asegurarte la mejor experiencia web."></cookie-law>
+
   </div>
 </template>
 
 <script>
   import CookieLaw from 'vue-cookie-law';
+  import { TipiIcon } from 'tipi-uikit';
 
   export default {
     name: "footer-block",
-    components: {CookieLaw}
+    components: {
+      CookieLaw,
+      TipiIcon
+    }
   }
 </script>
 
