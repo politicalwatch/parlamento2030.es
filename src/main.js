0d502a5f2e532bb392953b941dea9344fdd8fdd6
Alex Ahumada <alex.ahumada@politicalwatch.es>
2023-10-23 12:29:12 +0200

    Removes sentry

--- src/main.js
+++ src/main.js
@@ -7,17 +7,6 @@ import VueGtag from 'vue-gtag';
 import VueScrollTo from 'vue-scrollto';
 import '@politicalwatch/tipi-uikit/src/styles/main.scss';
 
-import * as Sentry from '@sentry/browser';
-import * as Integrations from '@sentry/integrations';
-
-let SENTRY_DSN = import.meta.env.VUE_APP_SENTRY_DSN;
-if (SENTRY_DSN) {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    integrations: [new Integrations.Vue({ Vue, attachProps: true })],
-  });
-}
-
 const app = createApp(App);
 
 app.use(router);
